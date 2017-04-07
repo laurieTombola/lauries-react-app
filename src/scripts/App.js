157a1c73e01f778720bb829870b06f78e8d47aac
Laurie Athey <laurie.athey@outlook.com>
2017-04-07 11:58:39 +0100

    moved files around, trying to set up bootstrap

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../images/logo.svg';
+import '../css/App.css';
 
 class App extends Component {
   render() {
@@ -8,11 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to my Laurie's React App</h2>
+          <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
